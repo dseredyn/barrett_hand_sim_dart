c0d00016dedc10173630790456f5560b70acd6b7
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-12-23 16:57:50 +0100

    script improved

--- scripts/gen_models.sh
+++ scripts/gen_models.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rosrun xacro xacro -o src/barrett_hand_robot/barrett_hand_defs/robots/barrett_hand.urdf src/barrett_hand_robot/barrett_hand_defs/robots/barrett_hand.urdf.xml
+barrett_hand_defs_dir=$(rospack find barrett_hand_defs)
+
+rosrun xacro xacro -o ${barrett_hand_defs_dir}/robots/barrett_hand.urdf ${barrett_hand_defs_dir}/robots/barrett_hand.urdf.xml
 
 rosrun xacro xacro -o src/barrett_hand_sim_dart/scenes/hook.urdf src/barrett_hand_sim_dart/scenes/hook.urdf.xacro
 rosrun barrett_hand_sim_dart barrett_hand_sim_dart_grasp barrett_hand_sim_dart /scenes/hook.urdf grasp_models/hook.xml
